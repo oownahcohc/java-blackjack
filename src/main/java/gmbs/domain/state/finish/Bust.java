d787d3ebad832d239114e944a17cd848e74db2ce
chanwoo <chocw0402@gmail.com>
2023-02-19 01:28:50 +0900

    Bust 클래스 - Dealer, Gamer 가 공통적으로 가질 수 있는 상태로, 카드 합이 21 이 넘은 상태

---
+++ src/main/java/gmbs/domain/state/finish/Bust.java
@@ -0,0 +1,24 @@
+package gmbs.domain.state.finish;
+
+import gmbs.domain.card.CardHand;
+import gmbs.domain.game.result.Result;
+import gmbs.domain.state.CardState;
+
+public final class Bust extends Finish {
+
+    private Bust(final CardHand cardHand) {
+        super(cardHand);
+    }
+
+    public static Bust from(final CardHand cardHand) {
+        return new Bust(cardHand);
+    }
+
+    @Override
+    public Result getCompareResult(CardState compareCardState) {
+        if (compareCardState.isBust()) {
+            return Result.DRAW;
+        }
+        return Result.LOSE;
+    }
+}
