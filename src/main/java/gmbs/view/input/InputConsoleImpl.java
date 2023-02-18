f40eda5a59438ef3402c3616404b79b837db906a
chanwoo <chocw0402@gmail.com>
2023-02-19 01:43:00 +0900

    feat : InputConsole 인터페이스 구현

---
+++ src/main/java/gmbs/view/input/InputConsoleImpl.java
@@ -0,0 +1,19 @@
+package gmbs.view.input;
+
+import java.util.Scanner;
+
+public class InputConsoleImpl implements InputConsole {
+
+    private static final String NAME_DELIMITER = ",";
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    @Override
+    public String[] readNames() {
+        return SCANNER.nextLine().split(NAME_DELIMITER);
+    }
+
+    @Override
+    public String readPossibilityOfDraw() {
+        return SCANNER.nextLine();
+    }
+}
