d7818d14796485f3ea0e79fa97ded3c44dd75a6b
chanwoo <chocw0402@gmail.com>
2023-02-19 01:44:18 +0900

    feat : Main 클래스

--- src/main/java/gmbs/Main.java
+++ src/main/java/gmbs/Main.java
@@ -1,7 +1,17 @@
 package gmbs;
 
+import gmbs.controller.BlackJackGameController;
+import gmbs.view.input.InputConsole;
+import gmbs.view.input.InputConsoleImpl;
+import gmbs.view.output.OutputConsole;
+import gmbs.view.output.OutputConsoleImpl;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InputConsole inputConsole = new InputConsoleImpl();
+        OutputConsole outputConsole = new OutputConsoleImpl();
+        BlackJackGameController blackJackGameController = new BlackJackGameController(inputConsole, outputConsole);
+        blackJackGameController.run();
     }
 }
