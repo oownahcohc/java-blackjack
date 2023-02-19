55a2d8a26090b664617cac3594626d8b3161b3b4
chanwoo <chocw0402@gmail.com>
2023-02-19 20:00:21 +0900

    refactor : Gamers 클래스 리팩토링 - BlackJackGame 에서 처리하던 gamers 의 게임 결과 처리 메서드 이동

--- src/main/java/gmbs/domain/player/Gamers.java
+++ src/main/java/gmbs/domain/player/Gamers.java
@@ -1,9 +1,13 @@
 package gmbs.domain.player;
 
 import gmbs.domain.card.vo.Card;
+import gmbs.domain.game.result.Results;
+import gmbs.domain.player.name.vo.Name;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Gamers {
 
@@ -50,6 +54,16 @@ public class Gamers {
 
     public Gamer getCurrentTurnPlayer() {
         return values.get(currentTurnPlayerIndex);
+    }
+
+    public Map<Name, Results> getGamerResults(Dealer dealer) {
+        return getValues().stream()
+                .collect(Collectors.toUnmodifiableMap(
+                        Gamer::getName,
+                        gamer -> Results.from(List.of(
+                                gamer.getPlayerResult(dealer.getCardState())
+                        ))
+                ));
     }
 
     public List<Gamer> getValues() {
