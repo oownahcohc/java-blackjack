e04d3db4fca9721b50e30284e6718e7e2408f3b9
chanwoo <chocw0402@gmail.com>
2023-09-24 02:43:49 +0900

    feat : CardState 인터페이스를 구현한 Drawable 클래스

---
+++ src/main/java/gmbs/domain/card/state/drawable/Drawable.java
@@ -0,0 +1,11 @@
+package gmbs.domain.card.state.drawable;
+
+import gmbs.domain.card.state.CardState;
+
+public class Drawable implements CardState {
+
+    @Override
+    public boolean canDraw() {
+        return true;
+    }
+}
