27d8066bb62878f6280be9a2be93b25e79d5c58c
chanwoo <chocw0402@gmail.com>
2023-09-24 02:42:08 +0900

    feat : Card 의 Pattern 필드 - enum 클래스

---
+++ src/main/java/gmbs/domain/card/vo/Pattern.java
@@ -0,0 +1,26 @@
+package gmbs.domain.card.vo;
+
+import java.util.List;
+
+public enum Pattern {
+
+    SPADE("스페이드"),
+    DIAMOND("다이아몬드"),
+    HEART("하트"),
+    CLOVER("클로버"),
+    ;
+
+    private final String value;
+
+    Pattern(final String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static List<Pattern> getAllPatterns() {
+        return List.of(values());
+    }
+}
